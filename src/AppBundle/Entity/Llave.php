ac22be2bb8a2074d70c2179a9d234cb075c01f73
Luis Ramón López López <lrlopez@gmail.com>
2019-10-30 10:31:54 +0100

    Añadidas anotaciones @var a las propiedades de Llave

--- src/AppBundle/Entity/Llave.php
+++ src/AppBundle/Entity/Llave.php
@@ -14,21 +14,25 @@ class Llave
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $descripcion;
 
     /**
      * @ORM\Column(type="string", length=10)
+     * @var string
      */
     private $codigo;
 
     /**
      * @ORM\Column(type="date", nullable=true)
+     * @var \DateTime
      */
     private $fechaPrestamo;
 }
