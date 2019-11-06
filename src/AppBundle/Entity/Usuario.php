67d2f4aff6bb432d07453fc5a836cd72a445a537
Luis Ramón López López <lrlopez@gmail.com>
2019-11-06 12:17:46 +0100

    Generadas anotaciones de la clase Usuario para convertirla en entidad

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -2,15 +2,38 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="usuario")
+ */
 class Usuario
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $nombre;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $apellidos;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $ordenanza;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
     private $secretario;
 }
