0838a76520c80fddab564d5c0a18c3e331f87bc3
Luis Ramón López López <lrlopez@gmail.com>
2019-11-06 12:22:35 +0100

    Implementadas las anotaciones de la entidad Usuario

--- src/AppBundle/Entity/Usuario.php
+++ src/AppBundle/Entity/Usuario.php
@@ -14,26 +14,112 @@ class Usuario
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $id;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $nombre;
 
     /**
      * @ORM\Column(type="string")
+     * @var string
      */
     private $apellidos;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var bool
      */
     private $ordenanza;
 
     /**
      * @ORM\Column(type="boolean")
+     * @var bool
      */
     private $secretario;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param string $apellidos
+     * @return Usuario
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOrdenanza()
+    {
+        return $this->ordenanza;
+    }
+
+    /**
+     * @param bool $ordenanza
+     * @return Usuario
+     */
+    public function setOrdenanza($ordenanza)
+    {
+        $this->ordenanza = $ordenanza;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSecretario()
+    {
+        return $this->secretario;
+    }
+
+    /**
+     * @param bool $secretario
+     * @return Usuario
+     */
+    public function setSecretario($secretario)
+    {
+        $this->secretario = $secretario;
+        return $this;
+    }
+
 }
