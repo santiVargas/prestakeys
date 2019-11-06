f79057eb4c4eadac5fac439684b14543f81cd325
Luis Ramón López López <lrlopez@gmail.com>
2019-11-06 12:14:28 +0100

    Creada la clase Usuario

---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Usuario
+{
+    private $id;
+
+    private $nombre;
+
+    private $apellidos;
+
+    private $ordenanza;
+
+    private $secretario;
+}
