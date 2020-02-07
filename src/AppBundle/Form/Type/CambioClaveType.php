3f5dc2a639223fcd5e8ea17017c15485c77fa428
Luis Ramón López López <lrlopez@gmail.com>
2020-02-07 14:10:04 +0100

    Confirmar la nueva contraseña con un doble campo

--- src/AppBundle/Form/Type/CambioClaveType.php
+++ src/AppBundle/Form/Type/CambioClaveType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form\Type;
 use AppBundle\Form\Model\CambioClave;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,8 +17,15 @@ class CambioClaveType extends AbstractType
             ->add('claveAntigua', PasswordType::class, [
                 'label' => 'Clave actual'
             ])
-            ->add('nuevaClave', PasswordType::class, [
-                'label' => 'Nueva clave'
+            ->add('nuevaClave', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Las dos contraseñas no coinciden',
+                'first_options'  => [
+                    'label' => 'Nueva contraseña'
+                ],
+                'second_options' => [
+                    'label' => 'Repita la nueva contraseña'
+                ],
             ]);
     }
 
