e93e197f3953123bfe21ef41ff97bba1dd916b81
Luis Ramón López López <lrlopez@gmail.com>
2020-02-07 14:34:29 +0100

    Los fixtures ahora codifican las contraseñas

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -3,14 +3,30 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use AppBundle\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Fixtures;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class LoadFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
+        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager, [
+            'providers' => [$this]
+        ]);
+    }
+
+    public function codificaClave($cadena)
+    {
+        return $this->encoder->encodePassword(new Usuario(), $cadena);
     }
 }
